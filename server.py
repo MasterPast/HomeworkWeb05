d18f08043078856419e7abfcb04152483e9f03f2
MasterPast <141021859+MasterPast@users.noreply.github.com>
2023-12-21 11:51:01 +0200

    start

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
+import asyncio
+import websockets
 
+async def hello(websockets):
+    name = await websockets.recv()
+    print(f'<<< {name}')
+
+    greeting = f'Hello {name}!!!'
+    await websockets.send(greeting)
+    print(f'>>> {greeting}')
+
+async def main():
+    async with websockets.serve(hello, 'localhost', 8765):
+        await asyncio.Future() 
+
+if __name__ == '__main__':
+    asyncio.run(main())   
